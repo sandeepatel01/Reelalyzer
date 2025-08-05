89811428b7e0bed44ad42a7d92d53c82862563b8
sandeepatel01 <sandeepatel.0101@gmail.com>
2025-08-06 00:11:54 +0530

    feat: reals dummy data for testing

---
+++ lib/constants.ts
@@ -0,0 +1,17 @@
+export const DEMO_REELS = [
+  {
+    id: 1,
+    title: "Tech Review Reel",
+    url: "https://www.instagram.com/reel/DMCK8BCBhcd",
+  },
+  {
+    id: 2,
+    title: "Travel Vlog Reel",
+    url: "https://www.instagram.com/reel/DLcjbWxzuiD",
+  },
+  {
+    id: 3,
+    title: "Fitness Tutorial",
+    url: "https://www.instagram.com/reel/DLmV6LrIcyn",
+  },
+];
