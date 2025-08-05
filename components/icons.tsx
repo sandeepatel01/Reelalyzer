cdaaf06a0119196865212524c16b4bbd7e39352c
sandeepatel01 <sandeepatel.0101@gmail.com>
2025-08-06 00:08:53 +0530

    feat: Add feature grid types card with custom icons - using Radix UI

---
+++ components/icons.tsx
@@ -0,0 +1,73 @@
+import { IconProps } from "@radix-ui/react-icons/dist/types";
+import * as React from "react";
+
+export const Icons = {
+  analytics: (props: IconProps) => (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M3 3v18h18" />
+      <path d="M18 17V9" />
+      <path d="M13 17V5" />
+      <path d="M8 17v-3" />
+    </svg>
+  ),
+  sentiment: (props: IconProps) => (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <circle cx="12" cy="12" r="10" />
+      <path d="M8 14s1.5 2 4 2 4-2 4-2" />
+      <line x1="9" y1="9" x2="9.01" y2="9" />
+      <line x1="15" y1="9" x2="15.01" y2="9" />
+    </svg>
+  ),
+
+  optimize: (props: IconProps) => (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 4v4m0 8v4m8-8h-4m-8 0H4m2.93-6.93l2.83 2.83m6.36 6.36l2.83 2.83m0-11.31l-2.83 2.83m-6.36 6.36L6.93 17.07"
+      />
+    </svg>
+  ),
+
+  compare: (props: IconProps) => (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M10 4H6a2 2 0 00-2 2v4m0 0l4-4m-4 4l4 4M14 20h4a2 2 0 002-2v-4m0 0l-4 4m4-4l-4-4"
+      />
+    </svg>
+  ),
+};
